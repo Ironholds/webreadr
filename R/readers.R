4897173428a0d21f69ab85895e1deb737824b59f
ironholds <ironholds@gmail.com>
2015-03-15 03:39:13 -0400

    add Apache Combined log format and associated combined_log.clf data file

--- R/readers.R
+++ R/readers.R
@@ -30,24 +30,82 @@
 #'@return a data.frame consisting of seven fields, as discussed above, with normalised
 #'timestamps.
 #'
+#'@seealso \code{\link{read_combined}} for the /Combined/ Log Format.
 #'@examples
 #'#Read in an example CLF-formatted file provided with \code{webtools}
 #'data <- read_clf(system.file("extdata/log.clf", package = "webtools"))
 #'@export
 read_clf <- function(file, has_header = FALSE){
   names <- c("ip_address", "remote_user_ident", "local_user_ident", "timestamp",
-             "request", "status_code")
+             "timestamp_junk","request", "status_code","bytes_sent")
   col_types <- list(col_character(),
                     col_character(),
                     col_character(),
-                    col_datetime("[%d/%b/%Y:%H:%M:%S %z]"),
+                    col_character(),
+                    col_character(),
                     col_character(),
                     col_integer(),
                     col_integer())
-  if(has_header){
-    return(read_delim(file = file, delim = " ", escape_backslash = FALSE, col_names = names,
-                      col_types = col_types, skip = 1))
-  }
-  return(read_delim(file = file, delim = " ", escape_backslash = FALSE, col_names = names,
-                    col_types = col_types))
+
+  data <- read_delim(file = file, delim = " ", escape_backslash = FALSE, col_names = names,
+                    col_types = col_types, skip = ifelse(has_header, 1, 0))
+  
+  #This is ugly. Due to CDF silliness ("oh, it's space separated! Except that one space that
+  #isn't a separator") we can't take full advantage of readr's awesome and have to do a fragment of it manually.
+  data$timestamp <- strptime(paste0(data$timestamp,data$timestamp_junk), format = "[%d/%b/%Y:%H:%M:%S %z]")
+  return(data[,!names(data) == "timestamp_junk"])
+}
+
+#'@title read Combined Log Format files
+#'@description read requests logs following the Combined Log Format.
+#'
+#'@details the Combined Log Format (CLF) is the same as the Common Log Format (CLF, because
+#'software engineers and naming go together like chalk and cheese), which
+#'is documented at \code{\link{read_clf}}. In addition to the fields described there,
+#'the Combined Log Format also includes:
+#'
+#'\itemize{
+#'  \item{referer:} {the referer associated with the request.}
+#'  \item{user_agent:} {the user agent of the user that made the request.}
+#'}
+#'
+#'\code{read_combined} handles these fields, as well as the CLF-standard ones. This is (amongst
+#'other things) the default logging format for \href{http://nginx.org/}{nginx} servers
+#'
+#'@seealso \code{\link{read_clf}} for the /Common/ Log Format.
+#'@examples
+#'#Read in an example Combined-formatted file provided with \code{webtools}
+#'data <- read_combined(system.file("extdata/combined_log.clf", package = "webtools"))
+#'@export
+read_combined <- function(file, has_header = FALSE){
+  names <- c("ip_address", "remote_user_ident", "local_user_ident", "timestamp",
+             "timestamp_junk","request", "status_code","bytes_sent","referer","user_agent")
+  col_types <- list(col_character(),
+                    col_character(),
+                    col_character(),
+                    col_character(),
+                    col_character(),
+                    col_character(),
+                    col_integer(),
+                    col_integer(),
+                    col_character(),
+                    col_character())
+  
+  data <- read_delim(file = file, delim = " ", escape_backslash = FALSE, col_names = names,
+                     col_types = col_types, skip = ifelse(has_header, 1, 0))
+  #See read_clf()
+  data$timestamp <- strptime(paste0(data$timestamp,data$timestamp_junk), format = "[%d/%b/%Y:%H:%M:%S %z]")
+  return(data[,!names(data) == "timestamp_junk"])
+}
+
+read_squid <- function(file, has_header = FALSE){
+  
+}
+
+read_varnish <- function(file, has_header = FALSE){
+  
+}
+
+read_aws <- function(file, has_header){
+  
 }
