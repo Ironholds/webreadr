b0629a44a349b90d12e1aa4df49da7842b5a4e3b
ironholds <ironholds@gmail.com>
2015-03-20 14:34:04 -0400

    expand unit tests

--- tests/testthat/test_encoding.R
+++ tests/testthat/test_encoding.R
@@ -1,5 +1,13 @@
 context("URL encoding tests")
 
+test_that("Check decoding handles spaces",{
+  expect_equal(url_decode("foo%20bar"),"foo bar")
+})
+
+test_that("Check decoding handles double quotes",{
+  expect_equal(url_decode("foo%22bar"),"foo\"bar")
+})
+
 test_that("Check decoding handles ASCII NULs", {
-  expect_that(url_decode("0;%20@%gIL"), equals("0; @"))
+  expect_equal(url_decode("0;%20@%gIL"), "0; @")
 })
