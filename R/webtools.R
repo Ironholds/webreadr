d74d3dbedd35a39b5aa88293c61481a235ceac12
ironholds <ironholds@gmail.com>
2015-03-21 14:46:16 -0400

    update docs, version properly

--- R/webtools.R
+++ R/webtools.R
@@ -1,4 +1,6 @@
-#' @title A library for reading, formatting and cleaning various common forms of web request log
+#' @title A library for reading, formatting and cleaning various common forms of web request log.
+#' @description see the \href{https://github.com/Ironholds/webtools/blob/master/vignettes/Introduction.Rmd}{introductory vignette}
+#' for more details!
 #' @name webtools
 #' @useDynLib webtools
 #' @importFrom Rcpp sourceCpp
